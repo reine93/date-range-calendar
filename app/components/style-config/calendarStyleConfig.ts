8b94d005fd627706031f6e05c498453e91bf6a97
rc05211 <renata.colak@dexcom.com>
2025-11-17 15:59:46 +0100

    feat: create calendar style config for tailwind classnames

---
+++ app/components/style-config/calendarStyleConfig.ts
@@ -0,0 +1,11 @@
+import { getDefaultClassNames, ClassNames } from "react-day-picker";
+
+export function getCalendarStyleConfig(): ClassNames {
+  const defaults = getDefaultClassNames();
+  console.log('debug class names', defaults)
+
+  return {
+    ...defaults,
+    root: `${defaults.root} p-4 bg-white rounded-xl shadow-md border border-slate-200 overflow-hidden`,
+  };
+}
